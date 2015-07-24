cff609af28a69f6f101d2e796844e82fbf8dd027
Dicey <diceytroop@gmail.com>
2015-07-23 21:56:32 -0400

    personas are now creatable / selectable

--- app/views/PersonaUI.jsx
+++ app/views/PersonaUI.jsx
@@ -6,6 +6,8 @@ var DropdownButton = require('react-bootstrap').DropdownButton;
 var MenuItem = require('react-bootstrap').MenuItem;
 var _ = require('underscore');
 var Button = require('react-bootstrap').Button;
+var ButtonInput = require('react-bootstrap').ButtonInput;
+var Input = require('react-bootstrap').Input;
 
 var PersonaUI = React.createClass({
 
@@ -26,15 +28,18 @@ var PersonaUI = React.createClass({
   addPersona: function() {
     var that = this;
     this.context.app.personaActionCreators.addPersona(this.state.new_persona_field_value);
-    this.setState({
-      new_persona_field_value: ""
-    })
+  },
+
+  changePersona: function(new_persona_id) {
+    this.context.app.personaActionCreators.setCurrentPersona(new_persona_id);
   },
 
   renderPersonaAdder: function() {
-    return( <div><input type="shortText" onChange={this.onTextChange} placeholder="New Persona"/>
-      <Button onClick={this.addPersona}>Add</Button>
-      </div>
+    return(<form>
+              <br></br>
+              <Input type="shortText" onChange={this.onTextChange} placeholder="New Persona"/>
+              <ButtonInput type="reset" onClick={this.addPersona}>Add</ButtonInput>
+      </form>
     )
   },
 
@@ -56,8 +61,8 @@ var PersonaUI = React.createClass({
 		var self = this;
     var menu_items = this.renderPersonaMenu(this.props.personas);
 		return (<Panel>
-              <div>Current Persona:
-                <DropdownButton title={this.props.current_persona}>
+              <div>Current Persona:<br/>
+                <DropdownButton title={this.props.current_persona} onSelect={this.changePersona}>
                   {menu_items}
                 </DropdownButton>
               </div>
